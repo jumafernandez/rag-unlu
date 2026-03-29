ad680f5a4014a14197202af8e6d05f3e86cc033e
Francisco Tamasi <frantamasi1@gmail.com>
2026-03-29 18:03:55 -0300

    revelar todas las secciones
    encontrar y clickear el boton que sirve para revelar todos las secciones que vamos a acceder

--- funciones.py
+++ funciones.py
@@ -9,13 +9,20 @@ from random import randint
 def descargar_Documento(opciones, url):
     
     driver = webdriver.Chrome(options=opciones)
-    driver.implicitly_wait(8)
+    driver.implicitly_wait(4)
     driver.get(url)
-    time.sleep(randint(1,5))
+    time.sleep(randint(1,3))
     
-    boton_ver_todos = driver.find_element(By.PARTIAL_LINK_TEXT, "Ocultar")
-    boton_ver_todos.click()
-    
+    botones = driver.find_elements(By.CSS_SELECTOR, ".md-button.ng-scope.md-ink-ripple")    #buscamos todos los botones
+    #por cada boton, buscamos el que sirve para ver todas las secciones
+    for boton in botones:
+        if "VER TODOS" in boton.text :
+            try:
+                boton.click()
+                break
+            except Exception:
+                print("No se pudo acceder al boton para acceder a todas las secciones...\n")
+
     
     lista = driver.find_elements(By.CSS_SELECTOR, "carpeta ng-isolate-scope _md")
     #for seccion in lista:
