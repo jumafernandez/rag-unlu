62288127c6371953e965695d2e8b76c725f83f2b
Francisco Tamasi <frantamasi1@gmail.com>
2026-04-02 16:47:52 -0300

    codigo listo para iniciar el scraping

--- funciones.py
+++ funciones.py
@@ -46,31 +46,7 @@ def encontrar_Carpeta(opciones, url, seccion):
     
     except Exception as e:
         print(f"Ocurrio un error inesperado al descargar los documentos de la carpeta: '{seccion}'...\n Error {e}")
-        
-        
-    """
-     lista = driver.find_elements(By.CSS_SELECTOR, ".pointer.ng-scope.ng-isolate-scope") #busca todas las carpetas de documentos
-
-    #por cada carpeta, buscamos cual coincide con la seccion que queres acceder
-
-    for elemento in lista:
-
-        if seccion in elemento.text:
-
-            try:
-
-                elemento.click()    #accedemos a la carpeta de documentos buscada
-
-                time.sleep(randint(1,5))
-
-                descargar_Documento(driver, seccion)    #iniciamos la descarga de documentos
-
-                break
-
-            except Exception:
-
-                print("No se pudo encontrar la seccion...\n")
-    """            
+                   
     
     
 def recorrer_pagina(driver, seccion):
@@ -109,6 +85,8 @@ def recorrer_pagina(driver, seccion):
                             #le cambiamos el nombre al archivo descargado
                             ruta_descarga = os.path.join(conf.DIRECTORIO_DESCARGAS, seccion)
                             op.renombrarArchivo(ruta_descarga, id_pdf)
+                            ruta_absoluta = os.path.join(conf.DIRECTORIO_DESCARGAS, f"{id_pdf}.pdf")
+                            print(f"PDF descargado en {ruta_absoluta}\n")
                             
                 except Exception as e:
                     print(f"Error procesando la fila {i+1}: {e}\n")    
@@ -138,75 +116,21 @@ def recorrer_pagina(driver, seccion):
     
     else:
         print(f"No hay paginas nuevas para recorrer en la seccion {seccion}...\n")    
-    
-    
-    """
-    print(f"Iniciando la descarga de los documentos de {seccion}\n")
-    time.sleep(5)
-    #calcular pagina
-    if ultima_pagina_recorrida(seccion, driver):
-    
-        while(True):
-            #recopilar elementos
-            lista = driver.find_elements(By.CSS_SELECTOR, "tr.md-row.ng-scope")
-            #accedemos a cada uno
-            for elemento in lista:
-            
-                id_pdf = str(uuid.uuid4())[:8]  #genera un id en hexa de 8 caracteres para que no se repitan los nombres de los pdf
-                time.sleep(randint(2,5))
-                #descargamos pdf
-                try:
-                    #accedemos donde se encuentra el documento
-                    click_PDF = elemento.find_element(By.CSS_SELECTOR, "i.fas.fa-arrow-circle-down.icon-button")
-                    #iniciamos la descarga
-                    click_PDF.click()
-                except NoSuchElementException:
-                    print("No se pudo iniciar la descarga del PDF...\n")
-                    id_pdf = "No se encontro archivo"
-
-                #le cambiamos el nombre al archivo descargado
-            
-                ruta_descarga = os.path.join(conf.DIRECTORIO_DESCARGAS, seccion)
-                op.renombrarArchivo(ruta_descarga, id_pdf)
-            
-                #recopilamos metadatos en un pandas
-            
-                recopilar_metadatos(elemento, id_pdf)
-            
-            #avanzamos de pagina
-            try:
-                click_avanzar = driver.find_element(By.CSS_SELECTOR, "button[aria-label='Next']")
-        
-                if click_avanzar.get_attribute("disabled"): #no hay mas paginas disponibles para recorrer
-                    op.guardarindice(seccion, nuevo_indice=(op.cargarindice(seccion) + 1))
-                    print(f"No hay mas paginas disponibles para recorrer dentro de la seccion {seccion}...\n")
-                    break 
-        
-                click_avanzar.click()
-                print("Siguiente pagina...\n")
-                
-                #actualizar archivo de pagina
-                op.guardarindice(seccion, nuevo_indice=(op.cargarindice(seccion) + 1))
-                time.sleep(5)
-
-            except NoSuchElementException:  #no encontro el boton para seguir
-                print(f"No hay mas paginas en la seccion {seccion}...\n")
-                break
-    
-    else:
-        print(f"No hay paginas nuevas para recorrer en la seccion {seccion}...\n")        
-    
-    """
+   
 
 def descargar_Documento(elemento, driver): #True si pudo descargar el documeto
     
-    time.sleep(randint(2,5))    
+        
     #descargamos pdf  
     try:
         #accedemos donde se encuentra el documento
         click_PDF = elemento.find_element(By.CSS_SELECTOR, "i.fas.fa-arrow-circle-down.icon-button")
         #iniciamos la descarga
-        driver.execute_script("arguments[0].click();", click_PDF)
+        driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", click_PDF)
+        time.sleep(0.5)
+        
+        click_PDF.send_keys(Keys.ENTER)
+        time.sleep(randint(2,5))
         return True
     
     except NoSuchElementException:
